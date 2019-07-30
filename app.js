08f9e94701fd9a6c80fb14d7d35faf014818bcdd
Pedro Gonzalez <pdro74@hotmail.com>
2019-07-30 23:04:22 +0200

    add MongoDB

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var apiRouter = require('./routes/api');
 
 var app = express();
+
+// execute database start file
+const db = require('./db')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,14 +26,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,22 @@
 let express = require('express');
 let router = express.Router();
 
-let Post = require('')
+let Post = require('../../models/post')
+
+// GET all posts: 
+router.get('/', (req, res) => {
+    Post.find((err, postsResult) => {
+        if (err) return res.json({ error: err })
+        res.json(postsResult)
+    })
+})
+
+// POST create new post:
+router.post('new', (req, res) => {
+    Post.create(req.body, (err, createdPost) => {
+        if (err) return res.json(err)
+        res.json(createdPost)
+    })
+})
 
 module.exports = router;
