b0b09323f4791d6d936e33f18e3be3678858a39c
Pedro Gonzalez <pdro74@hotmail.com>
2019-08-01 19:46:31 +0200

    small update

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ let mongoose = require('mongoose');
 
 let mongoDBUrl = 'mongodb://127.0.0.1/dbBlog'
 
-mongoose.connect(mongoDBUrl, { userNewUrlParser: true })
+mongoose.connect(mongoDBUrl, { useNewUrlParser: true })
 
 // capture error events
-
 let connection = mongoose.connection
+
 connection.on('error', (error) => {
     console.log(error);
 
